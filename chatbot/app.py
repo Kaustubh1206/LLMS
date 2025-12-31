9252577fc2342c22cd921dc116b4d5fdf6ab1ab2
Kautubh1206 <kaustubhgidh06@gmail.com>
2025-12-31 15:03:51 -0500

    chatbot

--- chatbot/app.py
+++ chatbot/app.py
@@ -2,47 +2,33 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-
-import streamlit as st 
-import os 
+import streamlit as st
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
-api_key = os.getenv("OPENAI_API_KEY")
-
-if not api_key:
-    raise ValueError("OPENAI_API_KEY not found. Check your .env file.")
-
-
-# # OPENAI API
 # os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
-
-# LANGSMITH TRACKING
-# For tracing and monitoring 
+## Langmith tracking
 os.environ["LANGCHAIN_TRACING_V2"]="true"
-
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
-
-## PROMPT TEMPLATE
+## Prompt Template
 
 prompt=ChatPromptTemplate.from_messages(
     [
-        ("system","You are a helpuful assistant. Please response to the user queries"),
+        ("system","You are a helpful assistant. Please response to the user queries"),
         ("user","Question:{question}")
     ]
 )
 
-#Streamlit 
+## streamlit framework
 
-st.title('Langchain Demo With OpenAI API ')
-input_text=st.text_input("Search the topic you want")
+st.title('Langchain Demo With OPENAI API')
+input_text=st.text_input("Search the topic u want")
 
-
-# open Ai LLMs
-llm=ChatOpenAI(model='gpt-3.5-turbo') #model
-output_parser=StrOutputParser() #responsible in getting output
+# openAI LLm 
+llm=ChatOpenAI(model="gpt-3.5-turbo")
+output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
 if input_text:
--- chatbot/localama.py
+++ chatbot/localama.py
@@ -1,47 +1,33 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate 
-from langchain_core.output_parsers import StrOutputParser # string parser output 
-# Whenever we are using third party we use community 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
-import streamlit as st 
-import os 
-from dotenv import load_dotenv 
+import streamlit as st
+import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
- 
-# # OPENAI API
-
-
-# LANGSMITH TRACKING
-# For tracing and monitoring 
 os.environ["LANGCHAIN_TRACING_V2"]="true"
-
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
-
-## PROMPT TEMPLATE
+## Prompt Template
 
 prompt=ChatPromptTemplate.from_messages(
     [
-        ("system","You are a helpuful assistant. Please response to the user queries"),
+        ("system","You are a helpful assistant. Please response to the user queries"),
         ("user","Question:{question}")
     ]
 )
+## streamlit framework
 
+st.title('Langchain Demo With LLAMA2 API')
+input_text=st.text_input("Search the topic u want")
 
-#Streamlit 
-
-st.title('Langchain Demo With OpenAI API ')
-input_text=st.text_input("Search the topic you want")
-
-
-
-# ollama LLMs
-llm=Ollama(model='llama3.2:3b') #model
-output_parser=StrOutputParser() #responsible in getting output
+# ollama LLAma2 LLm 
+llm=Ollama(model="llama2")
+output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
-
 if input_text:
-    st.write(chain.invoke({'question':input_text}))
+    st.write(chain.invoke({"question":input_text}))
